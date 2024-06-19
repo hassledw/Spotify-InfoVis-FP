6a0edc90c7f60b6b95d7cab3e2d9ce72eeba2c23
hassledw <dwh714@verizon.net>
2024-06-19 16:29:12 -0400

    fixed pie chart loading

--- main.py
+++ main.py
@@ -4,44 +4,46 @@ import pandas as pd
 '''
 Modified from starter code: https://go.plotly.com/website
 '''
+###################################
+#    Data Creation and Selection  #
+###################################
 
 df = pd.read_csv("./spotify.csv").dropna()
 categories = df['track_genre'].unique().tolist()
-
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash("Spotify Visualization", external_stylesheets=external_stylesheets, title="Spotify Visualization")
 
 # pie data
-alldf = df.value_counts('key').sort_index()
-popfilmdf = df[df['track_genre'] == "pop-film"].value_counts('key').sort_index()
-kpopdf = df[df['track_genre'] == "k-pop"].value_counts('key').sort_index()
-chilldf = df[df['track_genre'] == "chill"].value_counts('key').sort_index()
-saddf = df[df['track_genre'] == "sad"].value_counts('key').sort_index()
-grungedf = df[df['track_genre'] == "grunge"].value_counts('key').sort_index()
-indiandf = df[df['track_genre'] == "indian"].value_counts('key').sort_index()
-animedf = df[df['track_genre'] == "anime"].value_counts('key').sort_index()
-emodf = df[df['track_genre'] == "emo"].value_counts('key').sort_index()
-sertanejodf = df[df['track_genre'] == "sertanejo"].value_counts('key').sort_index()
-popdf = df[df['track_genre'] == "pop"].value_counts('key').sort_index()
-new_genre_df = pd.concat([alldf, popfilmdf, kpopdf, chilldf, saddf, grungedf, indiandf, animedf, emodf, sertanejodf, popdf], axis=1,
-                         keys=['all genres', 'pop-film', 'k-pop', 'chill', 'sad', 'grunge', 'indian', 'anime', 'emo', 'sertanejo', 'pop'])
+pie_genres = ["pop-film", "k-pop", "chill", "sad", "grunge", "indian", "anime", "emo", "sertanejo", "pop"]
+
+all_pie_df = pd.DataFrame()
+
+for genre in pie_genres:
+    genre_pie_df = df[df["track_genre"] == genre].value_counts('key', sort=True)
+    genre_pie_df = pd.DataFrame(genre_pie_df).rename(columns={"count" : genre})
+    all_pie_df = pd.concat([all_pie_df, genre_pie_df], axis=1).sort_index()
+
+########################
+#       App Layout     #
+########################
 
 app.layout = html.Div(children=[
     # graph 1: scatter
     html.Div(
         [html.H1(children='Spotify Dataset Scatter Visualization'),
     dcc.RadioItems(
-        id="pos-x",
+        id="feature-x",
         inline=True,
         options=df.columns,
         value=f"{df.columns[6]}",
     ),
     dcc.RadioItems(
-        id="pos-y",
+        id="feature-y",
         inline=True,
         options=df.columns,
         value=f"{df.columns[8]}",
     ),
-    dcc.Graph(id="graph"),
+    dcc.Graph(id="scatter-graph"),
     dcc.Slider(
         id="genre",
         min=0,
@@ -52,45 +54,59 @@ app.layout = html.Div(children=[
     ),
 ]),
     # graph 2: pie
-    html.Div(
-        [html.H1(children='Spotify Dataset Pie Visualization'),
-    dcc.Graph(id="pi-graph"),
-    html.P('Select Genre:'),
-    dcc.Dropdown(new_genre_df.columns, id='genre_dropdown', multi=False, clearable=False),
+    html.Div([
+        html.H1(children='Spotify Dataset Pie Visualization'),
+        dcc.Graph(id="pi-graph"),
+        html.P('Select Genre:'),
+        dcc.Dropdown(all_pie_df.columns,
+                 id='genre_dropdown',
+                 value=all_pie_df.columns[0],
+                 multi=False,
+                 clearable=False),
     ]),
 ])
 
+########################
+#       Callbacks      #
+########################
 @app.callback(
-    Output("graph", "figure"),
-    Input("pos-x", "value"),
-    Input("pos-y", "value"),
+    Output("scatter-graph", "figure"),
+    Input("feature-x", "value"),
+    Input("feature-y", "value"),
     Input("genre", "value"),
 )
-@app.callback(
-    Output("pi-graph", "figure"),
-    [Input("genre_dropdown", "value")]
-)
-
-def modify_graph(pos_x: str, pos_y: str, genre: int):
+def modify_graph(feature_x: str, feature_y: str, genre: int):
     fig = px.scatter(
         df[df['track_genre'] == categories[genre]],
-        x=pos_x,
-        y=pos_y,
+        x=feature_x,
+        y=feature_y,
         text="track_name",
         size_max=20,
-        title=f"{pos_x} vs {pos_y} on {categories[genre]} genre",
+        title=f"{feature_x} vs {feature_y} on {categories[genre]} genre",
     )
 
     fig.update_traces(marker=dict(size=10))
 
     return fig
 
-def modify_chart(genre_dropdown):
+@app.callback(
+    Output("pi-graph", "figure"),
+    [Input("genre_dropdown", "value")]
+)
+def modify_chart(genre):
     #p_labels = ['Key C', 'Key C#/Db', 'Key D', 'Key D#/Eb', 'Key E', 'Key F', 'Key F#/Gb', 'Key G', 'Key G#/Ab',
                 #'Key A', 'Key A#/Bb', 'Key B']
-    piechart = px.pie(data_frame=new_genre_df, names=genre_dropdown)
+
+    pie_genre = all_pie_df[genre]
+    # print(pie_genre)
+    piechart = px.pie(
+        data_frame=pie_genre,
+        values=pie_genre,
+        names=pie_genre.index
+    )
 
     return piechart
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
