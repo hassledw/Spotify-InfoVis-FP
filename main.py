930ffa28f3ea3ea6a2428d639328056b85670f58
hassledw <dwh714@verizon.net>
2024-06-20 17:50:42 -0400

    revamped scatter plot added more features

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
-'''
-Modified from starter code: https://go.plotly.com/website
-'''
+
 ###################################
 #    Data Creation and Selection  #
 ###################################
 
 df = pd.read_csv("./spotify.csv").dropna()
+df = df.drop(columns=["Unnamed: 0"])
 categories = df['track_genre'].unique().tolist()
+genre_artists = df[df['track_genre'] == categories[0]]["artists"].unique().tolist()
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash("Spotify Visualization", external_stylesheets=external_stylesheets, title="Spotify Visualization")
 
@@ -26,44 +27,99 @@ for genre in pie_genres:
 ########################
 #       App Layout     #
 ########################
-
-app.layout = html.Div(children=[
-    # graph 1: scatter
-    html.Div(
-        [html.H1(children='Spotify Dataset Scatter Visualization'),
-    dcc.RadioItems(
+scatter_x_input = dcc.Slider(
         id="feature-x",
-        inline=True,
-        options=df.columns,
-        value=f"{df.columns[6]}",
-    ),
-    dcc.RadioItems(
+        vertical=False,
+        min=0,
+        max=len(df.columns),
+        value=8,
+        step=1,
+        marks={i : column for i, column in enumerate(df.columns)}
+    )
+
+scatter_y_input =  dcc.Slider(
         id="feature-y",
-        inline=True,
-        options=df.columns,
-        value=f"{df.columns[8]}",
-    ),
-    dcc.Graph(id="scatter-graph"),
-    dcc.Slider(
-        id="genre",
+        vertical=True,
+        min=0,
+        max=len(df.columns),
+        value=5,
+        step=1,
+        marks={i : column for i, column in enumerate(df.columns)}
+    )
+
+scatter_graph = dcc.Graph(id="scatter-graph")
+
+genre_slider = dcc.Slider(
+        id="genre-slider",
         min=0,
         max=len(df["track_genre"].unique()),
         step=1,
         value=0,
-        marks={i: genre_name for i, genre_name in enumerate(categories)}
-    ),
-]),
-    # graph 2: pie
-    html.Div([
-        html.H1(children='Spotify Dataset Pie Visualization'),
-        dcc.Graph(id="pi-graph"),
-        html.P('Select Genre:'),
-        dcc.Dropdown(all_pie_df.columns,
+        marks={i: genre_name for i, genre_name in enumerate(categories) if i % 10 == 0}
+        # tooltip={
+        #     "always_visible": True,
+        #     "template": "categories[{value}]"
+        # }
+    )
+
+artist_range_slider = dcc.RangeSlider(
+    id="artist-range-slider",
+    min=0,
+    max=len(genre_artists),
+    step=1,
+    value=[0, len(genre_artists)],
+    marks={i : artist for i, artist in enumerate(genre_artists) if i % 100 == 0}
+)
+
+pi_graph = dcc.Graph(id="pi-graph")
+
+pi_genre_dropdown = dcc.Dropdown(all_pie_df.columns,
                  id='genre_dropdown',
                  value=all_pie_df.columns[0],
                  multi=False,
-                 clearable=False),
-    ]),
+                 clearable=False)
+
+app.layout = html.Div(children=[
+    # graph 1: scatter
+    dcc.Tabs([
+        dcc.Tab(label="Home", children=[
+            html.Div([
+                html.Br(),
+                html.H3(children='Spotify Dataset Scatter Visualization'),
+                html.Br(),
+                html.H6("Select a Desired Genre"),
+                genre_slider,
+                html.H6("Select Desired Artists"),
+                artist_range_slider,
+                html.Div([
+                    scatter_y_input
+                ], style={"width": "15%", "display": "inline-block", "position": "relative"}),
+                html.Div([
+                    scatter_graph
+                ], style={"width": "85%", "display": "inline-block", "position": "relative"}),
+                html.Div([
+                    scatter_x_input
+                ], style={"width": "85%", "float": "right"})
+            ]),
+
+            html.Br(),
+            # graph 2: pie
+            html.Div([
+                html.Br(),
+                html.H3(children='Spotify Dataset Pie Visualization'),
+                pi_graph,
+                html.P('Select Genre:'),
+                pi_genre_dropdown
+            ]),
+        ]),
+
+        dcc.Tab(label="Waveform Analysis", children=[
+            html.Div([
+                html.Br(),
+                html.H1(children='Spotify Dataset Waveform Visualization')
+            ])
+        ])
+    ])
 ])
 
 ########################
@@ -73,16 +129,32 @@ app.layout = html.Div(children=[
     Output("scatter-graph", "figure"),
     Input("feature-x", "value"),
     Input("feature-y", "value"),
-    Input("genre", "value"),
+    Input("genre-slider", "value"),
+    Input("artist-range-slider", "value")
 )
-def modify_graph(feature_x: str, feature_y: str, genre: int):
+def modify_scatter_graph(feature_x: int, feature_y: int, genre: int, artist_range: list):
+    genre_df = df[df['track_genre'] == categories[genre]]
+    genre_artists = genre_df["artists"].unique().tolist()
+    artists = [genre_artists[i] for i in range(artist_range[0], artist_range[1])]
+
     fig = px.scatter(
-        df[df['track_genre'] == categories[genre]],
-        x=feature_x,
-        y=feature_y,
-        text="track_name",
+        genre_df[genre_df["artists"].isin(artists)],
+        x=df.columns[feature_x],
+        y=df.columns[feature_y],
+        hover_data={
+            'artists': True,
+            'track_name': True
+        },
+        color="artists",
         size_max=20,
-        title=f"{feature_x} vs {feature_y} on {categories[genre]} genre",
+        title=f"{df.columns[feature_x]} vs {df.columns[feature_y]} on {categories[genre]} genre",
+    )
+
+    fig.update_layout(
+        title_font_size=30,
+        title_font_color="red",
+        xaxis_title_font_color="green",
+        yaxis_title_font_color="green"
     )
 
     fig.update_traces(marker=dict(size=10))
@@ -90,10 +162,25 @@ def modify_graph(feature_x: str, feature_y: str, genre: int):
     return fig
 
 @app.callback(
+Output("artist-range-slider", "marks"),
+    Output("artist-range-slider", "value"),
+    Output("artist-range-slider", "max"),
+    [Input("genre-slider", "value")]
+)
+def modify_artist_slider_labels(genre: int):
+    genre_artists = df[df['track_genre'] == categories[genre]]["artists"].unique().tolist()
+    marks = {i : artist for i, artist in enumerate(genre_artists) if i % 30 == 0}
+    value = [0, len(genre_artists)]
+    max = len(genre_artists)
+
+    return marks, value, max
+
+
+@app.callback(
     Output("pi-graph", "figure"),
     [Input("genre_dropdown", "value")]
 )
-def modify_chart(genre):
+def modify_chart(genre : str):
     #p_labels = ['Key C', 'Key C#/Db', 'Key D', 'Key D#/Eb', 'Key E', 'Key F', 'Key F#/Gb', 'Key G', 'Key G#/Ab',
                 #'Key A', 'Key A#/Bb', 'Key B']
 
